86002a052610f0984d99e576e7ab063ff4af8bd7
shouta0715 <Myheroes0715@gmail.com>
2024-05-24 10:43:24 +0900

    add: repples

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,42 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import p5 from 'p5';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const sketch = (p: p5) => {
+  let ripples: { x: number; y: number; radius: number; alpha: number }[] = [];
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  p.setup = () => {
+    p.createCanvas(window.innerWidth, window.innerHeight);
+    p.noFill();
+  };
+
+  p.draw = () => {
+    console.log('draw');
+    p.background(255);
+
+    // 波紋を描画
+    for (let i = ripples.length - 1; i >= 0; i--) {
+      let r = ripples[i];
+      console.log(r);
+      p.stroke(0, 0, 0, r.alpha);
+      p.ellipse(r.x, r.y, r.radius * 10, r.radius * 10);
+      r.radius += 2;
+      r.alpha -= 4;
+
+      if (r.alpha <= 0) {
+        ripples.splice(i, 1);
+      }
+    }
+  };
+
+  p.mousePressed = () => {
+    // マウスをクリックした場所に波紋を追加
+    let ripple = {
+      x: p.mouseX,
+      y: p.mouseY,
+      radius: 0,
+      alpha: 255,
+    };
+    ripples.push(ripple);
+  };
+};
+
+new p5(sketch);

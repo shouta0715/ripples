733a2b8a9b4be6d8c4ec565742374c168acc4b37
shouta0715 <Myheroes0715@gmail.com>
2024-05-25 00:06:08 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,12 @@ const sketch = (p: p5) => {
   };
 
   p.draw = () => {
-    console.log('draw');
     p.background(255);
 
     // 波紋を描画
     for (let i = ripples.length - 1; i >= 0; i--) {
       let r = ripples[i];
-      console.log(r);
+
       p.stroke(0, 0, 0, r.alpha);
       p.ellipse(r.x, r.y, r.radius * 10, r.radius * 10);
       r.radius += 2;
@@ -28,13 +27,14 @@ const sketch = (p: p5) => {
   };
 
   p.mousePressed = () => {
-    // マウスをクリックした場所に波紋を追加
     let ripple = {
       x: p.mouseX,
       y: p.mouseY,
       radius: 0,
       alpha: 255,
     };
+
+    console.log(ripple);
     ripples.push(ripple);
   };
 };
